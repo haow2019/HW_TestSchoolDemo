3d7ea4c140719c07692dc85632357acef3ea695a
haow2019 <haow2010@163.com>
2019-06-13 23:36:52 +0800

    update files

--- run_all.py
+++ run_all.py
@@ -3,14 +3,14 @@ import unittest
 import HTMLTestRunner
 
 # 用例地址
-test_dir = "D:\\Selenium_study\\TestSchoolPage\\testCases"
+test_dir = r"./testCases"
 
 # discover方法加载多个用例集合
 discover = unittest.defaultTestLoader.discover \
     (start_dir=test_dir, pattern="test*.py", top_level_dir=None)
 
 # 测试报告地址
-fileName = "D:\\Selenium_study\\TestSchoolPage\\testReport\\" + "TestResult.html"
+fileName = r"testReport/TestResult.html"
 fp = open(fileName, "wb")
 runner = HTMLTestRunner.HTMLTestRunner(stream=fp,
                                        title=u"TestReport",
